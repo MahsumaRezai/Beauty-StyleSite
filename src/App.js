00358e8d0e78687d3aa135ae0fc4d03bea67b124
Masoumeh Rezai <you@example.com>
2023-08-22 02:46:15 +0430

    add all image

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import { Fragment } from "react";
 
 
 function App() {
-    
+
     return (
         <Fragment>
-           <h1>hello</h1>
+            <h1>hello</h1>
         </Fragment>
     )
 }
