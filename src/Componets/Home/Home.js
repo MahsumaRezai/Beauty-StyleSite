f4b7181bae99050e02fae3c105159d32f4406bb9
Masoumeh Rezai <you@example.com>
2023-08-22 07:23:25 +0430

    add home style

--- src/Componets/Home/Home.js
+++ src/Componets/Home/Home.js
@@ -5,11 +5,10 @@ import img1 from '../image/img.png'
 const Home = (props) => {
     return (
         <Fragment>
-            <h1 className={classes.title}>Beauty Style</h1>
             <div className={classes.home}>
                 <div className={classes.image}>
                     <img src={img1} alt="" />
-                    <button className="btn">Read More</button>
+                    <h1 className={classes.title}>Beauty Style For Fahsion</h1>
 
                 </div>
 
