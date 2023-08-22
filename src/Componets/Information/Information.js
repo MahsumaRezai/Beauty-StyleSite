8160e30ee5b9b99f5d85c067e6d77361f5120620
Masoumeh Rezai <you@example.com>
2023-08-23 00:20:36 +0430

    add about for inaformation

---
+++ src/Componets/Information/Information.js
@@ -0,0 +1,25 @@
+import { Fragment } from "react";
+
+const Information = (props) => {
+    return (
+        <Fragment>
+            <div>
+                <div>
+                    <h1>About Us</h1>
+                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                     labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+
+                </div>
+                <div>
+                    
+
+                </div>
+
+
+
+
+            </div>
+        </Fragment>
+    )
+}
+export default Information;
