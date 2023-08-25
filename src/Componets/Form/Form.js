f8d00997f7f1c769512a33a8d3fb1075affe73ed
Masoumeh Rezai <you@example.com>
2023-08-25 05:33:19 +0430

    use ref form form section

--- src/Componets/Form/Form.js
+++ src/Componets/Form/Form.js
@@ -1,4 +1,4 @@
-import { Fragment, useRef } from "react";
+import { Fragment, useRef, useState } from "react";
 import img from '../image/right.png';
 import classes from './Form.module.css'
 import Button from "./Button";
@@ -13,6 +13,10 @@ const Form = (props) => {
         const nameEnterd = NameCheck.current.value;
         const passwordEnterd = PasswordCheck.current.value;
         const emailEnterd = EmailCheck.current.value;
+        if (nameEnterd.trim().length === 0 || passwordEnterd.trim().length === 0 || emailEnterd.trim().length === 0) {
+            console.log('ddkdk')
+
+        }
 
 
 
@@ -23,11 +27,11 @@ const Form = (props) => {
                 <div className={classes.img}>
                     <img alt="" src={img} />
                 </div>
-                <form className={classes.content} >
+                <form className={classes.content} onSubmit={FormHandler} >
                     <h1 className={classes.title}>Get In Touch</h1>
-                    <input placeholder="Name" />
-                    <input placeholder="Phone Number" />
-                    <input placeholder="Email" />
+                    <input placeholder="Name" ref={NameCheck} />
+                    <input placeholder="Phone Number" ref={PasswordCheck} />
+                    <input placeholder="Email" ref={EmailCheck} />
                     <textarea className={classes.text}></textarea>
                     <Button>Send Me</Button>
 
